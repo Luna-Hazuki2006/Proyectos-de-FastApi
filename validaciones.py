532ea3246a99a692d6ff8e28e5ebe88b29d64edd
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-05-22 11:59:26 -0400

    Con validación de email

--- validaciones.py
+++ validaciones.py
@@ -1,17 +1,34 @@
 from pydantic import BaseModel, ValidationError, field_validator, Field
 from typing import Optional
+from datetime import datetime
+from email_validator import validate_email, EmailNotValidError
 
 class Usuario(BaseModel): 
     usuario : str
     contraseña : str = Field(min_length=4)
     correo : str
-    edad : Optional[int] = None
+    edad : int
 
     @field_validator('usuario')
-    def tamaño(cls, usuario): 
-        if len(usuario) < 4: raise ValueError('La longitud mínima es de 4 caracteres')
-        elif len(usuario) > 20: raise ValueError('La longitud máxima es de 20 caracteres')
+    def nombre(cls, usuario): 
+        if len(usuario) <= 0: raise ValueError('El nombre de usario no puede estar vacío')
         return usuario
+    @field_validator('edad')
+    def vejez(cls, edad): 
+        if edad < 18: raise ValueError('La no puede ser menor a 18')
+        return edad
+    @field_validator('contraseña')
+    def tamaño(cls, contraseña): 
+        if len(contraseña) < 5: raise ValueError('La longitud mínima de la contraseña es de 5 caracteres')
+        return contraseña
+    @field_validator('correo')
+    def validacion(cls, correo): 
+        try: 
+            validado = validate_email(correo)
+            correo = validado.email
+            return correo
+        except EmailNotValidError as e: 
+            raise ValueError('El email no es válido')
     
 class Item(BaseModel): 
     nombre: str
@@ -19,6 +36,14 @@ class Item(BaseModel):
     precio: float
     impuesto: float | None = None
 
+class Producto(BaseModel): 
+    Id : int
+    nombre : str
+    descripcio : str
+    precio : float
+    categoria : str
+    creacion : datetime = datetime.now()
+    actualizacion : datetime | None
 
 user = Usuario(usuario='LunaHazuki', contraseña='1234', correo='lunahazuki2006@outlook.com', edad=13)
 user2 = Usuario(usuario='LunaHazuki', contraseña='1234', correo='lunahazuki2006@outlook.com')
